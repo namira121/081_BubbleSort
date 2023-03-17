90a28c9feb8e487a31eeec63fd9867a188dbddc5
namira121 <127211008+namira121@users.noreply.github.com>
2023-03-17 16:35:38 +0700

    menambahkan void display

--- BubbleSort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort/BubbleSort.cpp
@@ -44,4 +44,16 @@ void unsorted() {
 	for (int j = 0; j < n; j++) {				// Looping dengan j dimulai dari 0 hingga n-1
 		cout << a[j] << endl;					// Output ke layar
 	}
+	cout << endl;
+}
+
+void display() {			// prosedur untuk menampilkan hasil
+	cout << endl;							// Output baris kosong
+	cout << "=================================" << endl; //Output ke layar
+	cout << "Element Array yang telah tersusun" << endl; //Output ke layar
+	cout << "=================================" << endl; //Output ke layar
+	for (int j = 0; j < n; j++) {			// Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;				// Output ke layar
+	}
+	cout << endl;							// Output baris kosong
 }
